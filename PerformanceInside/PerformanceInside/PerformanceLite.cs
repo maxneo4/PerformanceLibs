4e776431b96d335ca2bbaf16c18bce99a396e581
Edwin Alejandro Benitez Soriano <Edwin.Benitez@bizagi.com>
2016-05-05 06:56:28 -0500

    Separando escritura en otra clase y colocando contingencia a Trace.Write para el caso que falle el archivo en disco

--- PerformanceInside/PerformanceInside/PerformanceLite.cs
+++ PerformanceInside/PerformanceInside/PerformanceLite.cs
@@ -1,59 +1,75 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
 
 namespace Neo.PerformanceInside
 {
     public class PerformanceLite
     {
+        #region static Fields
+        public static string OutPutFolder
+        {
+            get; set;
+        }
+        #endregion
+
         #region Fields
 
-        readonly Stopwatch _stopWatch;
-        string _outPutFolder;
-        string _reportName;
+        Stopwatch _stopWatch;    
         string _tab;
         string _headers;
         string _row;
-        string _pathReport;
+        PerformanceLiteFileOutPut _outPutWriter;
+        IOutPut _alternativeOutPutWriter;
 
         #endregion
 
         #region Constructor
-
-        public PerformanceLite(string outPutFolder, string reportName)
+                
+        public PerformanceLite(string reportName, Action<string> alternativeActionIfErrorFile = null)
         {
+            if (alternativeActionIfErrorFile == null)
+                alternativeActionIfErrorFile = (s) => Trace.Write(s);
+            _outPutWriter = new PerformanceLiteFileOutPut(OutPutFolder, reportName);            
+            _alternativeOutPutWriter = new PerformanceLiteActionOutPut(alternativeActionIfErrorFile);
             _stopWatch = new Stopwatch();
-            _outPutFolder = outPutFolder;
             _tab = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator;
             _headers = string.Format("Description{0}Minutes{0}Seconds{0}MiliSeconds{0}TotalMiliSeconds{1}", _tab, Environment.NewLine);
-            _row = "{1}{0}{2}{0}{3}{0}{4}{0}{5}"+Environment.NewLine;
-            _reportName = string.Format("{0}_{1}.csv", reportName, DateTime.Now.ToString("ddMMMyyyy HH-mm"));
-            _pathReport = Path.Combine(_outPutFolder, _reportName);
+            _row = "{1}{0}{2}{0}{3}{0}{4}{0}{5}" + Environment.NewLine;
+        }  
+
+        #endregion
+
+        #region Public methods 
+
+        public void BeginCount()
+        {
+            Write(_headers);
+            _stopWatch.Start();
         }
 
-    #endregion
+        public void CountTime(string description)
+        {
+            _stopWatch.Stop();
+            string row = string.Format(_row, _tab, description, _stopWatch.Elapsed.Minutes, _stopWatch.Elapsed.Seconds, _stopWatch.Elapsed.Milliseconds, _stopWatch.ElapsedMilliseconds);
+            Write(row);
+            _stopWatch.Restart();
+        }
 
-    #region Public methods 
+        public void OpenReport()
+        {
+            _outPutWriter.OpenReport();
+        }
 
-    public void BeginCount()
-    {
-        File.AppendAllText(_pathReport, _headers);
-        _stopWatch.Start();
-    }
+        #endregion
 
-    public void CountTime(string description)
-    {
-        _stopWatch.Stop();
-        string row = string.Format(_row, _tab, description, _stopWatch.Elapsed.Minutes, _stopWatch.Elapsed.Seconds, _stopWatch.Elapsed.Milliseconds, _stopWatch.ElapsedMilliseconds);
-        File.AppendAllText(_pathReport, row);
-        _stopWatch.Restart();
-    }
+        #region Private methods
 
-    public void OpenReport()
-    {
-        Process.Start(_pathReport);
-    }
-    #endregion
+        private void Write(string text)
+        {
+            if (!_outPutWriter.Write(text))
+                _alternativeOutPutWriter.Write(text);
+        }
 
+        #endregion
     }
 }
---
+++ PerformanceInside/PerformanceInside/PerformanceLiteOutPut.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Neo.PerformanceInside
+{
+
+    #region Interface OutPut
+    internal interface IOutPut
+    {
+        void Write(string text);
+    }
+
+    internal class PerformanceLiteActionOutPut : IOutPut
+    {
+        Action<string> _action;
+
+        public PerformanceLiteActionOutPut(Action<string> action)
+        {
+            _action = action;
+        }
+
+        public void Write(string text)
+        {
+            _action(text);
+        }
+    }
+    #endregion
+
+    internal class PerformanceLiteFileOutPut
+    {          
+        string _reportName;  
+        string _pathReport;        
+
+        public PerformanceLiteFileOutPut(string outPutFolder, string reportName)
+        {
+            _reportName = string.Format("{0}_{1}.csv", reportName, DateTime.Now.ToString("ddMMMyyyy HH-mm"));
+            _pathReport = Path.Combine(outPutFolder, _reportName);
+        }
+
+        public bool Write(string text)
+        {
+            try
+            {
+                File.AppendAllText(_pathReport, text);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void OpenReport()
+        {
+            Process.Start(_pathReport);
+        }
+    }    
+}
