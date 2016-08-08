65115210b118596a1ef6b59a588623ac746143c5
Alejandro Benitez <maxneo4@gmail.com>
2016-08-08 18:35:00 -0500

    Add hours column, add memory count possibility, updating release dll. Add static way to store and get performanceLite instances

--- PerformanceInside/PerformanceInside/PerformanceLite.cs
+++ PerformanceInside/PerformanceInside/PerformanceLite.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Neo.PerformanceInside
@@ -10,31 +11,47 @@ namespace Neo.PerformanceInside
         {
             get; set;
         }
+
+        private static Dictionary<string,PerformanceLite> _instances = new Dictionary<string, PerformanceLite>();
+
+        public static PerformanceLite GetInstance(string key)
+        {
+            return _instances[key];
+        }
+
+        public static void SetInstance(string key, PerformanceLite performanceLite)
+        {
+            _instances[key] = performanceLite;
+        }
         #endregion
 
         #region Fields
 
-        Stopwatch _stopWatch;    
+        Stopwatch _stopWatch;
+        StopMemory _stopMemory;        
         string _tab;
         string _headers;
         string _row;
         PerformanceLiteFileOutPut _outPutWriter;
         IOutPut _alternativeOutPutWriter;
 
+        public bool CountMemory { get; set; }
+
         #endregion
 
         #region Constructor
-                
+
         public PerformanceLite(string reportName, Action<string> alternativeActionIfErrorFile = null)
         {
             if (alternativeActionIfErrorFile == null)
                 alternativeActionIfErrorFile = (s) => Trace.Write(s);
             _outPutWriter = new PerformanceLiteFileOutPut(OutPutFolder, reportName);            
             _alternativeOutPutWriter = new PerformanceLiteActionOutPut(alternativeActionIfErrorFile);
-            _stopWatch = new Stopwatch();
+            _stopWatch = new Stopwatch();            
+            _stopMemory = new StopMemory();
             _tab = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator;
-            _headers = string.Format("Description{0}Minutes{0}Seconds{0}MiliSeconds{0}TotalMiliSeconds{1}", _tab, Environment.NewLine);
-            _row = "{1}{0}{2}{0}{3}{0}{4}{0}{5}" + Environment.NewLine;
+            _headers = string.Format("Description{0}Hours{0}Minutes{0}Seconds{0}MiliSeconds{0}TotalMiliSeconds{0}Memory(kb){1}", _tab, Environment.NewLine);
+            _row = "{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}" + Environment.NewLine;
         }  
 
         #endregion
@@ -45,14 +62,20 @@ namespace Neo.PerformanceInside
         {
             Write(_headers);
             _stopWatch.Start();
+            if(CountMemory)
+                _stopMemory.Start();
         }
 
         public void CountTime(string description)
         {
             _stopWatch.Stop();
-            string row = string.Format(_row, _tab, description, _stopWatch.Elapsed.Minutes, _stopWatch.Elapsed.Seconds, _stopWatch.Elapsed.Milliseconds, _stopWatch.ElapsedMilliseconds);
+            if (CountMemory)
+                _stopMemory.Stop();
+            string row = string.Format(_row, _tab, description, _stopWatch.Elapsed.Hours, _stopWatch.Elapsed.Minutes, _stopWatch.Elapsed.Seconds, _stopWatch.Elapsed.Milliseconds, _stopWatch.ElapsedMilliseconds, _stopMemory.Memory);
             Write(row);
             _stopWatch.Restart();
+            if (CountMemory)
+                _stopMemory.Restart();
         }
 
         public void OpenReport()
